543a9873ba0c58d11b8784b735a33c538f0c889a
Maciek Makowski <maciek.makowski@gmail.com>
2012-10-21 19:28:57 +0100

    changed round-robin to skew

--- 01_heap_trees/1.4_round-robin_heaps.hs
+++ 01_heap_trees/1.4_round-robin_heaps.hs
@@ -1,9 +1,6 @@
 module RoundRobinHeaps where
 
-data Colour = Blue | Red
-  deriving Show
-
-data Tree a = Null | Fork Colour a (Tree a) (Tree a)
+data Tree a = Null | Fork a (Tree a) (Tree a)
   deriving Show
 
 isEmpty :: Tree a -> Bool
@@ -11,13 +8,13 @@ isEmpty Null = True
 isEmpty _    = False
 
 minElem :: Tree a -> a
-minElem (Fork _ e _ _) = e
+minElem (Fork e _ _) = e
 
 deleteMin :: Ord a => Tree a -> Tree a
-deleteMin (Fork _ e l r) = merge l r
+deleteMin (Fork e l r) = merge l r
 
 insert :: Ord a => a -> Tree a -> Tree a
-insert e t = merge (Fork Blue e Null Null) t
+insert e t = merge (Fork e Null Null) t
 
 merge :: Ord a => Tree a -> Tree a -> Tree a
 merge Null t = t
@@ -26,6 +23,23 @@ merge t1 t2
   | minElem t1 <= minElem t2 = join t1 t2
   | otherwise                = join t2 t1
 
+-- skew heap: we always insert to the left subtree, but after doing so replace left with right
+-- so that next time we'll instert to the other sub-tree; this is equivalent to a 
+-- round-robin heap
+
 join :: Ord a => Tree a -> Tree a -> Tree a
-join (Fork Blue e l r) t = Fork Red e (merge l t) r
-join (Fork Red e l r) t = Fork Blue e l (merge r t)
+join (Fork e l r) t = Fork e r (merge l t)
+
+{-
+## Exercise 1.5
+
+1) 
+
+(insert 2 . insert 1 . insert 4 . insert 3 . insert 6 . insert 5 . insert 8 . insert 7 . insert 9) Null
+
+will look the same when skew heap is used.
+
+2)
+
+can't produce it from atomic nodes?
+-}
