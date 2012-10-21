01d720ced5d2b3ce09da10263bb8292333aee6e3
Maciek Makowski <maciek.makowski@gmail.com>
2012-10-21 10:51:23 +0100

    reorganised to better support multiple sub-chapters

--- 01_heap_trees/1.1_binary_heap_trees.hs
+++ 01_heap_trees/1.1_binary_heap_trees.hs
@@ -1,4 +1,4 @@
-module HeapTrees where
+module BinaryHeapTrees where
 
 data Tree a = Null | Fork a (Tree a) (Tree a)
   deriving Show
@@ -27,19 +27,28 @@ merge t1 t2
   | minElem t1 <= minElem t2 = join t1 t2
   | otherwise                = join t2 t1
 
+-- there is at least 6 straightforward ways in which we can implement join:
+
+join1 (Fork e l r) t = Fork e l (merge r t)
+join2 (Fork e l r) t = Fork e r (merge l t)
+join3 (Fork e l r) t = Fork e t (merge l r)
+join4 (Fork e l r) t = Fork e (merge r t) l
+join5 (Fork e l r) t = Fork e (merge l t) r
+join6 (Fork e l r) t = Fork e (merge l r) t
+
 {- 
 Exercise 1.1
 
-1) `join (Fork e l r) t = Fork e l (merge r t)` produces rightist linear tree and
-5) `join (Fork e l r) t = Fork e (merge l t) r` produces leftist linear tree.
+* `join1` produces rightist linear tree and
+* `join5` produces leftist linear tree.
 
-E.g. 1) can be proven by assuming there is going to be a left branch in a tree that results
+E.g. `join1` can be proven by assuming there is going to be a left branch in a tree that results
 from a series of joins. If so, there must have been a join in the series that first
-introduced a left branch. But a left branch is always constructed from the left branch
+introduced a left branch; but a left branch is always constructed from the left branch
 of the first input tree, so it must have existed before -- contradiction.
 -}
 
 -- let's pick strategy 2) for now:
 join :: Ord a => Tree a -> Tree a -> Tree a
-join (Fork e l r) t = Fork e r (merge l t)
+join = join2
 
